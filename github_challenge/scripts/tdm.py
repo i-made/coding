fd21d330077d4353d68fcf725c09aa9fa991e5c1
Nikhil Kulkarni <nikhil2kulkarni@gmail.com>
2017-02-13 16:04:08 -0500

    modification to the extractor

--- github_challenge/scripts/tdm.py
+++ github_challenge/scripts/tdm.py
@@ -1,9 +1,13 @@
-# -*- coding: UTF-8 -*-
-#!/usr/bin/env python
 '''
-Author: Nikhil Kulkarni (nikhil.kulkarni@innoplexus.com)
-Usage: python tdm.py
+Author : Nikhil Kulkarni
+Pupose : Gihub Data Science Interview
+Date   : February 11, 2017
+Desc   : Generates Document Term Matrix (DTM)
+Data   : Requires wikipedia data JSON file
+Run    : python dtm.py wikidata.json
+Read   : Generated DTM has [Documents x Terms] with (tf * idf) freq
 '''
+
 import re
 import sys
 import json
@@ -11,143 +15,16 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame
 
-from sklearn.cluster import KMeans
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.metrics import silhouette_samples, silhouette_score
-
-words = [
-    "similarity",
-    "gensim",
-    "data-science",
-    "math",
-    "prediction",
-    "hadoop-platform",
-    "representation",
-    "analytical",
-    "neural-network",
-    "big-data",
-    "natural-language-processing",
-    "recommendation-systems",
-    "evaluation",
-    "spark-stream",
-    "natural-language",
-    "cluster",
-    "scientific-computing",
-    "tensorflow",
-    "ai",
-    "scipy",
-    "pyspark",
-    "scientist",
-    "data-engineer",
-    "dimention",
-    "natural-language-algorithm",
-    "precision",
-    "map-reduce",
-    "numpy",
-    "linear-regression",
-    "graphical-model",
-    "spark-shell",
-    "tensor",
-    "supervised-learning",
-    "probabilistic",
-    "data-engineering",
-    "spark",
-    "cosine-distance",
-    "c#",
-    "pca",
-    "csharp",
-    "probabilistic-graphical-models",
-    "modeling",
-    "statistical",
-    "regression",
-    "apache-spark",
-    "extract",
-    "metrics",
-    "python",
-    "mapr",
-    "mr",
-    "fscore",
-    "python2",
-    "recall",
-    "application",
-    "developer",
-    "networkx",
-    "principal-component-analysis",
-    "ica",
-    "probabilistic-models",
-    "c-sharp",
-    "recommendation-system",
-    "analytics",
-    "scientific",
-    "etl",
-    "mapreduce",
-    "lda",
-    "engineer",
-    "transform",
-    "react",
-    "distance",
-    "logistic-regression",
-    "graphx",
-    "neural-net",
-    "software",
-    "load",
-    "word2vec",
-    "artificial-intelligence",
-    "javascript",
-    "deep-learning",
-    "hadoop",
-    "python3",
-    "extract-load-transform",
-    "information-retrieval",
-    "graph",
-    "graphical-models",
-    "signal",
-    "f-score",
-    "matrix",
-    "features",
-    "apachespark",
-    "f-measure",
-    "dimension",
-    "mllib",
-    "unsupervised-learning",
-    "r",
-    "word-embeddings",
-    "embeddings",
-    "vector",
-    "hadoop-cluster",
-    "data-scientist",
-    "recommendations",
-    "clustering",
-    "hadoop-mr",
-    "data-processing",
-    "ml",
-    "machine-learning",
-    "reinforcement-learning",
-    "latent-dirichlet-allocation",
-    "mrjob",
-    "natural-language-process",
-    "nlp",
-    "f1score",
-    "pgm",
-    "cnn",
-    "convolutional-neural-network"
-]
+# imports variable words which is the list of input entities
+# i.e. words = ['c#','machine-learning','csharp',....]
+from entities import words
 
 
-def clean_tdm(df):
-    "Cleans TDM by removing first column and columns with all zeros"
+def clean_dtm(df):
+    "Cleans TDM by removing columns with all zeros"
     ddf = df.astype(float, errors='ignore')
     ddf = df.loc[(df.sum(axis=1) != 0), (df.sum(axis=0) != 0)]
-    # L, S, svd = pcp(ddf, verbose=True, svd_method="exact")
-    # print L
-    # print S
-    # ddf.to_csv('new_file.csv', index='pubmed_id')
     return ddf
-
-
-def get_coocc(data_matrix):
-    "Returns cooccurence matrix : X'X "
-    return data_matrix.T.dot(data_matrix)
 
 
 def get_tfidf_matrix(data_matrix):
@@ -158,33 +35,34 @@ def get_tfidf_matrix(data_matrix):
     return df
 
 
-def get_clusters(df, k):
-    "Returns clusters against each document"
-    mat = df.as_matrix()
-    km = KMeans(n_clusters=k)
-    km.fit(mat)
-    labels = km.labels_
-    silhouette_avg = silhouette_score(mat, labels)
-    results = pd.DataFrame([df.index, labels]).T
-    return results, silhouette_avg
+class DTM():
 
-
-class TDM():
+    # This class handles generation of Document term matrix
+    # It requires a dictionary where key is a term and value is the document
+    # i.e. data = { "csharp": "C-Sharp is a programming ....", "nlp": "..."...}
 
     def __init__(self):
         # data = pd.read_csv('./Wiki_Data.csv')
+
+        # wikidata is the dictionary file with terms as keys and docs as values
         with open('wikidata.json') as json_data:
             data = json.load(json_data)
+
         self.title_list = ['Doc:' + i.split()[0] for i in data.values()]
         self.abstract_list = data.values()
         self.entity_list = [i.replace('-', ' ') for i in words]
-        print len(self.entity_list)
+
+        # print len(self.entity_list)
 
     def numpy_pandas_csvwriter(self):
+
+        # Initiates empty matrix
         rows = len(self.abstract_list)
         columns = len(self.entity_list)
         term_matrix = np.zeros([columns, rows])
+
         print 'Constructing %d X %d Matrix' % (columns, rows)
+
         for abs_idx, abstract in enumerate(self.abstract_list):
             print abs_idx, len(self.abstract_list)
             for ent_idx, entity in enumerate(self.entity_list):
@@ -198,20 +76,33 @@ class TDM():
                 except Exception as e:
                     number = 0.0
                 term_matrix[ent_idx][abs_idx] = number
+
+        # Generaes pandas dataframe
         df = self.pandas_to_csv(term_matrix.transpose())
         return df
 
     def pandas_to_csv(self, matrix):
         index_name = 'Entity'
+
         rows = len(self.abstract_list)
         columns = len(self.entity_list)
+
         df = pd.DataFrame(
             matrix,
             columns=[entity for entity in self.entity_list],
             index=[entity for entity in self.title_list]
         )
         df.index.name = index_name
-        df.to_csv('TDM_%d_%d.csv' % (rows, columns), index=True)
+
+        # title_df = pd.DataFrame(df['Entity'])
+        # df = df.set_index('Entity')
+        # df = df.apply(lambda x: pd.to_numeric(x, errors='ignore'))
+        # df = clean_tdm(df)
+        # df = get_tfidf_matrix(df)
+        # df = pd.concat([title_df, df], axis=1)
+
+        # Outputs a Document Term matrix
+        df.to_csv('DTM_%d_%d.csv' % (rows, columns), index=True)
         print '\n\nDone.'
         return df
 
@@ -219,18 +110,3 @@ class TDM():
 if __name__ == '__main__':
     tdm_extraction = TDM()
     tdm_extraction.numpy_pandas_csvwriter()
-
-    # CSV_PATH = sys.argv[1]
-    # df = pd.read_csv(CSV_PATH)
-    # # df = pd.concat(tp, ignore_i)
-    # title_df = pd.DataFrame(df['Entity'])
-    # df = df.set_index('Entity')
-    # df = df.apply(lambda x: pd.to_numeric(x, errors='ignore'))
-    # df = clean_tdm(df)
-    # df = get_tfidf_matrix(df)
-    # # df = get_coocc(df)
-    # # df = pd.concat([title_df, df], axis=1)
-    # df.to_csv('final.csv')
-    # # for i in range(2, 20):
-    # # results, score = get_clusters(clean_tdm(df), 4)
-    # # print i, score
--- github_challenge/scripts/wikipedia_data_extractor.py
+++ github_challenge/scripts/wikipedia_data_extractor.py
@@ -1,151 +1,68 @@
+'''
+Author : Nikhil Kulkarni
+Pupose : Gihub Data Science Interview
+Date   : February 11, 2017
+Desc   : Extracts wikipedia data given entities
+Data   : Requires keywords.csv file which has entities
+Run    : python wikipedia_data_extractor.py
+Read   : Generates a json file in 'data_files' folder with wiki text
+'''
+
 import re
+import sys
 import json
 import pandas as pd
+
+# Wiki api initialization
 from wikiapi import WikiApi
-wiki = WikiApi()
 wiki = WikiApi({'locale': 'en'})
 
-words = [
-    "similarity",
-    "gensim",
-    "data-science",
-    "math",
-    "prediction",
-    "hadoop-platform",
-    "representation",
-    "analytical",
-    "neural-network",
-    "big-data",
-    "natural-language-processing",
-    "recommendation-systems",
-    "evaluation",
-    "spark-stream",
-    "natural-language",
-    "cluster",
-    "scientific-computing",
-    "tensorflow",
-    "ai",
-    "scipy",
-    "pyspark",
-    "scientist",
-    "data-engineer",
-    "dimention",
-    "natural-language-algorithm",
-    "precision",
-    "map-reduce",
-    "numpy",
-    "linear-regression",
-    "graphical-model",
-    "spark-shell",
-    "tensor",
-    "supervised-learning",
-    "probabilistic",
-    "data-engineering",
-    "spark",
-    "cosine-distance",
-    "c#",
-    "pca",
-    "csharp",
-    "probabilistic-graphical-models",
-    "modeling",
-    "statistical",
-    "regression",
-    "apache-spark",
-    "extract",
-    "metrics",
-    "python",
-    "mapr",
-    "mr",
-    "fscore",
-    "python2",
-    "recall",
-    "application",
-    "developer",
-    "networkx",
-    "principal-component-analysis",
-    "ica",
-    "probabilistic-models",
-    "c-sharp",
-    "recommendation-system",
-    "analytics",
-    "scientific",
-    "etl",
-    "mapreduce",
-    "lda",
-    "engineer",
-    "transform",
-    "react",
-    "distance",
-    "logistic-regression",
-    "graphx",
-    "neural-net",
-    "software",
-    "load",
-    "word2vec",
-    "artificial-intelligence",
-    "javascript",
-    "deep-learning",
-    "hadoop",
-    "python3",
-    "extract-load-transform",
-    "information-retrieval",
-    "graph",
-    "graphical-models",
-    "signal",
-    "f-score",
-    "matrix",
-    "features",
-    "apachespark",
-    "f-measure",
-    "dimension",
-    "mllib",
-    "unsupervised-learning",
-    "r",
-    "word-embeddings",
-    "embeddings",
-    "vector",
-    "hadoop-cluster",
-    "data-scientist",
-    "recommendations",
-    "clustering",
-    "hadoop-mr",
-    "data-processing",
-    "ml",
-    "machine-learning",
-    "reinforcement-learning",
-    "latent-dirichlet-allocation",
-    "mrjob",
-    "natural-language-process",
-    "nlp",
-    "f1score",
-    "pgm",
-    "cnn",
-    "convolutional-neural-network"
-]
 
-res = dict()
-for i in words:
-    print 'searched for ', i
-    results = wiki.find(i)
-    print results
-    print '*' * 70
-    if results:
-        text = ''
-        for j, q in enumerate(results):
-            print results[j]
-            article = wiki.get_article(results[j])
-            t = article.content.encode('ascii', 'ignore')
-            print t
-            print '*' * 60
-            t = " ".join(t.split())
-            print '*' * 60
-            text += ' ' + t
-            print '-' * 60
-            print text
-        text = text.replace(',', ' ')
-        text = text.strip()
-        res[i] = text
-data = pd.DataFrame(res.items(), columns=['Entity', 'Text'])
-data.to_csv('WikiData.csv')
-with open("wikidata.json", "w") as file:
-    file.write(json.dumps(res, file, indent=4))
+def get_entities_from_csv(file_data):
+    words = list(set(file_data[file_data.columns[0]]))
+    return words
+
+
+def extract_wiki_data(entities):
+    # iterates over entities and fetches wikipedia data
+    res = dict()
+    for i in words:
+        print 'Fetching data for: ', i
+        results = wiki.find(i)
+        if results:
+            # Api returns multiple matches, this part combines them into one
+            text = ''
+            for j, q in enumerate(results):
+                article = wiki.get_article(results[j])
+                t = article.content.encode('ascii', 'ignore')
+                t = " ".join(t.split())
+                text += ' ' + t
+            # Combined text of all possible terms is assigned
+            text = text.replace(',', ' ')
+            text = text.strip()
+            res[i] = text
+    return res
+
+
+def json_file_writer(res, filename="data_files/wikidata.json"):
+    # Dumps data into json/csv file
+    data = pd.DataFrame(res.items(), columns=['Entity', 'Text'])
+    # data.to_csv('WikiData.csv')
+    with open(filename, "w") as file:
+        file.write(json.dumps(res, file, indent=4))
+
+
+def main():
+    # reads keywords.csv file
+    entity_file_name = sys.argv[1]
+    file_data = pd.read_csv(entity_file_name)
+    # words variable stores entities in a list
+    words = get_entities_from_csv(file_data)
+    # res has wikipedia corpus in a dictionary
+    res = extract_wiki_data(words)
+    # writes the data to a json file in data_files folder
+    json_file_writer(res)
+    return res
+
+if __name__ == '__main__':
+    main()
